c0c302f3bc3a84d64a6115fac23908f3e09de5a0
Дмитрий <dsrelax@mail.ru>
2025-04-04 23:53:57 +0400

    NuGet create settings

--- nanoFramework.MessagePack/Properties/AssemblyInfo.cs
+++ nanoFramework.MessagePack/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CSharp.TestApplication")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("nanoFramework MessagePack serealizer/deserializer library")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("CSharp.TestApplication")]
