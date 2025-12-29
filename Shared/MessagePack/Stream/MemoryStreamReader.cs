c3171b56ce0b3ebfeddebeb7d8fc25fb562c31a9
Дмитрий <dsrelax@mail.ru>
2025-12-30 00:18:45 +0400

    perf(MemoryStreamReader.cs): Removed unnecessary memory allocation and copying

--- Shared/MessagePack/Stream/MemoryStreamReader.cs
+++ Shared/MessagePack/Stream/MemoryStreamReader.cs
@@ -5,7 +5,6 @@
 using System;
 using System.IO;
 #endif
-using System.Collections;
 using nanoFramework.MessagePack.Dto;
 using nanoFramework.MessagePack.Utility;
 
@@ -14,15 +13,11 @@ namespace nanoFramework.MessagePack.Stream
     internal sealed class MemoryStreamReader : BaseReader, IDisposable
     {
         private readonly MemoryStream _stream;
-        private readonly ArrayList _bytesGatheringBuffer;
-
-        private long _bytesGatheringBufferLength = 0;
-        private bool _bytesGatheringInProgress;
+        private long _gatheringStartPosition;
 
         public MemoryStreamReader(MemoryStream stream)
         {
             _stream = stream;
-            _bytesGatheringBuffer = new ArrayList();
         }
 
         public override byte ReadByte()
@@ -33,42 +28,18 @@ namespace nanoFramework.MessagePack.Stream
                 throw ExceptionUtility.NotEnoughBytes(0, 1);
             }
 
-            var result = (byte)temp;
-            if (_bytesGatheringInProgress)
-            {
-                _bytesGatheringBuffer.Add(new byte[] { result });
-                _bytesGatheringBufferLength++;
-            }
-
-            return result;
+            return (byte)temp;
         }
 
         public override ArraySegment ReadBytes(uint length)
         {
             var buffer = ReadBytesInternal(length);
-
-            if (_bytesGatheringInProgress)
-            {
-                _bytesGatheringBuffer.Add(buffer);
-                _bytesGatheringBufferLength += buffer.Length;
-            }
-
             return buffer;
         }
 
         public override void Seek(long offset, SeekOrigin origin)
         {
-            if (_bytesGatheringInProgress)
-            {
-                var buffer = ReadBytesInternal((uint)offset);
-
-                _bytesGatheringBuffer.Add(buffer);
-                _bytesGatheringBufferLength += buffer.Length;
-            }
-            else
-            {
-                _stream.Seek(offset, origin);
-            }
+            _stream.Seek(offset, origin);
         }
 
         public void Dispose()
@@ -92,16 +63,14 @@ namespace nanoFramework.MessagePack.Stream
         {
             if (_stream.Position <= _stream.Length)
             {
-                _bytesGatheringInProgress = false;
-
-                byte[] result = new byte[_bytesGatheringBufferLength];
-
-                int destinationOffset = 0;
-                foreach (byte[] part in _bytesGatheringBuffer)
-                {
-                    Array.Copy(part, 0, result, destinationOffset, part.Length);
-                    destinationOffset += part.Length;
-                }
+                long currentPosition = _stream.Position;
+                long bytesGathered = currentPosition - _gatheringStartPosition;
+                
+                byte[] result = new byte[bytesGathered];
+                
+                _stream.Position = _gatheringStartPosition;
+                _stream.Read(result, 0, result.Length);
+                _stream.Position = currentPosition;
 
                 return new ArraySegment(result, 0, result.Length);
             }
@@ -113,9 +82,7 @@ namespace nanoFramework.MessagePack.Stream
 
         protected override void StartTokenGathering()
         {
-            _bytesGatheringInProgress = true;
-            _bytesGatheringBuffer.Clear();
-            _bytesGatheringBufferLength = 0;
+            _gatheringStartPosition = _stream.Position;
         }
     }
 }
